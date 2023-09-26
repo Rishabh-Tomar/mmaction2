6192751cf495e7651cc09cf26232e7fffb78721a
rishabh <rishabh.tomar@yahoo.com>
2023-09-26 21:34:09 +0530

    for results

--- demo/demo_spatiotemporal_det.py
+++ demo/demo_spatiotemporal_det.py
@@ -348,7 +348,7 @@ def main():
     results = []
     for human_detection, prediction in zip(human_detections, predictions):
         results.append(pack_result(human_detection, prediction, new_h, new_w))
-        print(f"\nDetected action: {prediction[0]}  Prediction score:{prediction[1]}")
+        print(f"\nDetected action: {prediction[0][0][0]}  Prediction score:{prediction[0][0][1]}")
 
     
     
